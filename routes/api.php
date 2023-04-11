a635460336d7cce3eca35414bc9703a3efc96c03
KatowProject <naufalfaqih443@gmail.com>
2023-04-11 11:51:14 +0700

    partial update

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RecipeController;
-use App\Models\User;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
